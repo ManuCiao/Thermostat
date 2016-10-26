4c21d2a3b5498723cf32d2e721179ac70c714b51
ManuCiao <mn.sabatino@gmail.com>
2016-10-26 15:29:08 +0100

    add increaseTemperature

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -7,3 +7,19 @@ Thermostat = function(temperature = DEFAULT_TEMPERATURE) {
   this._powerOn = true;
   this._temperature = temperature;
 };
+
+Thermostat.prototype.increaseTemperature = function (number) {
+    if (powerOn == true) {
+      this._temperature += number {
+        
+        if (this._temperature >= POWER_ON_LIMIT) {
+          alert ("It cannot go above 20 degrees")
+      }
+    } else if (this._temperature >= POWER_OFF_LIMIT){
+      alert ("it cannot go above 32 degrees")
+    } else {
+      this._temperature += number
+      console.log(this._temperature);
+    }
+    }
+};
