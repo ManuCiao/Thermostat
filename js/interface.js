866a8112c0626db54ea93c0cfcdb5ecc9b60f2b4
ManuCiao <mn.sabatino@gmail.com>
2016-10-28 02:31:51 +0100

    update the CS S-part2

--- js/interface.js
+++ js/interface.js
@@ -5,14 +5,11 @@ $(document).ready(function() {
     event.preventDefault();
     var city = $('#current-city').val();
     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=7fd8be3aa80d32da727582fffc53b9bd&units=metric', function(data) {
-      $('#Temperature').text(data.main.temp);
+      $('#Temp').text(data.main.temp);
     });
   });
 
-
-
-
-  $('#Temperature').text(thermostat._temperature);
+  $('#Temp').text(thermostat._temperature);
 
   $('#increaseTemperature').on('click', function() {
     thermostat.increaseTemperature(1);
@@ -42,8 +39,8 @@ $(document).ready(function() {
   });
 
   function updateTemperature() {
-    $('#Temperature').text(thermostat._temperature);
-    $('#Temperature').attr('class', thermostat.tellUsUsage());
+    $('#Temp').text(thermostat._temperature);
+    $('#Temp').attr('class', thermostat.tellUsUsage());
     $('#celsius').attr('class', thermostat.tellUsUsage());
     // $('#title').attr('class', thermostat.tellUsUsage());
 
