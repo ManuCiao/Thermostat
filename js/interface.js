e43ca905ed630381c3364e34797a078853526029
ManuCiao <mn.sabatino@gmail.com>
2016-10-28 01:15:36 +0100

    update the CSS

--- js/interface.js
+++ js/interface.js
@@ -1,21 +1,16 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
 
-displayWeather('London');
-
- function displayWeather(city) {
-    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
-    var token = '&appid=7fd8be3aa80d32da727582fffc53b9bd';
-    var units = '&units=metric';
-    $.get(url + token + units, function(data) {
-      $('#current-temperature').text(data.main.temp);
-  });
-
   $('#select-city').submit(function(event) {
     event.preventDefault();
     var city = $('#current-city').val();
-    displayWeather(city);
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=7fd8be3aa80d32da727582fffc53b9bd&units=metric', function(data) {
+      $('#Temperature').text(data.main.temp);
+    });
   });
+
+
+
 
   $('#Temperature').text(thermostat._temperature);
 
@@ -36,19 +31,22 @@ displayWeather('London');
 
   $('#powerModeOn').click(function() {
     thermostat.powerModeOn();
-    $('#isPowerModeOn').text('ON');
+    $('#isPowerModeOn').text('Power Mode On');
     updateTemperature();
   });
 
   $('#powerModeOff').click(function() {
     thermostat.powerModeOff();
-    $('#isPowerModeOn').text('OFF');
+    $('#isPowerModeOn').text('Power Mode Off');
     updateTemperature();
   });
 
   function updateTemperature() {
     $('#Temperature').text(thermostat._temperature);
     $('#Temperature').attr('class', thermostat.tellUsUsage());
+    $('#celsius').attr('class', thermostat.tellUsUsage());
+    // $('#title').attr('class', thermostat.tellUsUsage());
+
   };
 
- });
+});
