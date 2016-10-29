c1e5487566ca52e7838fb9eea7ad2a62b78fe2b6
ManuCiao <mn.sabatino@gmail.com>
2016-10-29 20:00:36 +0200

    Add color to usage-while entering the location the color of the temperature cannot update

--- js/interface.js
+++ js/interface.js
@@ -16,38 +16,49 @@ $(document).ready(function() {
   $('#increaseTemperature').on('click', function() {
     thermostat.increaseTemperature(1);
     updateTemperature();
-
+    updateUsage();
   });
 
   $('#decreaseTemperature').on('click', function() {
     thermostat.decreaseTemperature(1);
     updateTemperature();
+    updateUsage();
   });
 
   $('#resetTemperature').click(function() {
     thermostat.resetTemperature();
     updateTemperature();
+    updateUsage();
   });
 
   $('#powerModeOn').click(function() {
     thermostat.powerModeOn();
     $('#isPowerModeOn').text('Power Mode On');
     updateTemperature();
+    updateUsage();
   });
 
   $('#powerModeOff').click(function() {
     thermostat.powerModeOff();
     $('#isPowerModeOn').text('Power Mode Off');
     updateTemperature();
+    updateUsage();
   });
 
   function updateTemperature() {
     $('#Temp').text(thermostat._temperature);
     $('#Temp').attr('class', thermostat.tellUsUsage());
     $('#celsius').attr('class', thermostat.tellUsUsage());
+  }
+  function updateUsage(){
     var $x = $('#legendUsage').attr('class', thermostat.tellUsUsage());
     console.log($x.text(thermostat.tellUsUsage()));
-
+    if(thermostat.tellUsUsage() === 'low-usage') {
+      $('#legendUsage').css('color', 'green');
+    } else if(thermostat.tellUsUsage() === 'medium-usage') {
+      $('#legendUsage').css('color', 'orange');
+    } else {
+      $('#legendUsage').css('color', 'red');
+    }
   }
-
 });
