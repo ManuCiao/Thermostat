de7871488bd78cdccac8ee7338795f026f89f0e4
ManuCiao <mn.sabatino@gmail.com>
2016-10-30 19:49:31 +0100

    update some functions and the css

--- js/interface.js
+++ js/interface.js
@@ -1,17 +1,24 @@
 $(document).ready(function() {
+
   var thermostat = new Thermostat();
+  updateTemperature();
+  updateUsage();
+  getWeatherData(city);
 
-    $('#select-city').submit(function(event) {
-      event.preventDefault();
-      var $results = $('#city'),
-      city = $('#current-city').val();
-      $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=7fd8be3aa80d32da727582fffc53b9bd&units=metric', function(data) {
-        $('#Temp').text(Math.round(data.main.temp));
-    });
-      $results.text('The temperature in ' + city.toUpperCase() + ' is:');
-    });
+  $('#select-city').on('submit', function(e) {
+    e.preventDefault();
+    var $results = $('#city'),
+    city = $('#current-city').val();
+    getWeatherData(city);
+  });
 
-  $('#Temp').text(thermostat._temperature);
+  function getWeatherData(city) {
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=7fd8be3aa80d32da727582fffc53b9bd&units=metric', function(data) {
+      $('#city').text('The temperature in ' + city.toUpperCase() + ' is: ' + Math.round(data.main.temp)).append('<small>°C</small>');
+      $('#city img').remove();
+      $('#city').append('<img src="http://openweathermap.org/img/w/' + data.weather[0].icon +'.png">');
+    });
+  }
 
   $('#increaseTemperature').on('click', function() {
     thermostat.increaseTemperature(1);
@@ -47,12 +54,12 @@ $(document).ready(function() {
 
   function updateTemperature() {
     $('#Temp').text(thermostat._temperature);
-    $('#Temp').attr('class', thermostat.tellUsUsage());
-    $('#celsius').attr('class', thermostat.tellUsUsage());
+    $('#Temp').attr('class', thermostat.tellUsUsage()).append('<small>°C</small>');
   }
+
   function updateUsage(){
-    var $x = $('#legendUsage').attr('class', thermostat.tellUsUsage());
-    console.log($x.text(thermostat.tellUsUsage()));
+    var $usage = $('#legendUsage').attr('class', thermostat.tellUsUsage());
+    $usage.text(thermostat.tellUsUsage());
     if(thermostat.tellUsUsage() === 'low-usage') {
       $('#legendUsage').css('color', 'green');
     } else if(thermostat.tellUsUsage() === 'medium-usage') {
